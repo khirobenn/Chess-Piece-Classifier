bfd6384f571debf11a73e17485b78b51a9aeb17a
khirobenn <khireddine.benmeziane@etu.sorbonne-universite.fr>
2026-01-27 17:32:55 +0100

    Creating prediction post

---
+++ api/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, UploadFile
+import uvicorn
+import tensorflow as tf
+import numpy as np
+import imageio.v3 as iio
+
+class_names = ['Bishop', 'King', 'Knight', 'Pawn', 'Queen', 'Rook']
+
+img_width = 224
+img_height = 224
+
+model = tf.keras.models.load_model("../chess_pieces_recognizer_model.keras")
+
+app = FastAPI()
+
+@app.get("/ping")
+async def ping():
+    return "Hello, server is running !"
+
+@app.post("/predict")
+async def predict(file: UploadFile):
+    img_bytes = await file.read()
+    image = iio.imread(img_bytes, index=None)
+    img_array = np.expand_dims(image, axis=0)
+    img_array = tf.image.resize(img_array, [img_height,img_width])
+    return class_names[np.argmax(model.predict(img_array))]    
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
